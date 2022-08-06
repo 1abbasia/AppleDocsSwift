00e43f61aa2c8a5033506f17e2f26a85449013dd
Ahsan Abbasi <1abbasia@gmail.com>
2022-08-05 20:45:59 -0400

    added button for screen timer

--- Scrumdinger/Scrumdinger/MeetingView.swift
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -12,15 +12,26 @@ struct MeetingView: View {
         VStack {
             ProgressView(value: 5, total: 15)
             HStack {
-                VStack {
+                VStack(alignment: .leading) {
                     Text("seconds elapsed")
+                        .font(.caption)
                     Label("300", systemImage: "hourglass.bottomhalf.fill")
                 }
-                VStack {
+                Spacer()
+                VStack(alignment: .trailing) {
                     Text("Seconds Remaining")
+                        .font(.caption)
                     Label("600", systemImage: "hourglass.tophalf.fill")
                 }
             }
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack{
+                Text("Speaker 1 of 3")
+                Button(action: {}){
+                    Image(systemName: "forward.fill")
+                }
+            } 
         }
     }
 }
