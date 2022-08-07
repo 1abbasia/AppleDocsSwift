c17023fdf20477b36378d14d7bfab21528763c2e
Ahsan Abbasi <1abbasia@gmail.com>
2022-08-06 21:21:52 -0400

    fixed foreach looping issue

--- Scrumdinger/Scrumdinger/Model/DailyScrum.swift
+++ Scrumdinger/Scrumdinger/Model/DailyScrum.swift
@@ -15,8 +15,13 @@ struct DailyScrum {
     var theme: Theme
 }
 
+
+
 extension DailyScrum {
-    static let sampleData: [DailyScrum] = [
-        DailyScrum(title: "design", attendees: ["Alfa", "Bravo", "Charlie", "Delta"], lengthInMinutes: 10, theme: .yellow)
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, theme: .yellow),
+        DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .orange),
+        DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy)
     ]
 }
---
+++ Scrumdinger/Scrumdinger/Model/ScrumsView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Ahsan on 8/6/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct ScrumsView:  View{
+    let scrums: [DailyScrum]
+    var body: some View {
+        List {
+            ForEach(scrums, id: \.title){scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View{
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
