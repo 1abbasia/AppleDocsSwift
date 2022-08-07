209c021ac3bde2b76b79972421859578793b388e
Ahsan Abbasi <1abbasia@gmail.com>
2022-08-06 23:43:51 -0400

    buld verified to swipe from row to scrum title view

--- Scrumdinger/Scrumdinger/Model/ScrumsView.swift
+++ Scrumdinger/Scrumdinger/Model/ScrumsView.swift
@@ -19,7 +19,13 @@ struct ScrumsView:  View{
                 }
                 .listRowBackground(scrum.theme.mainColor)
             }
-        }
+        }.navigationTitle("Daily Scrums")
+            .toolbar{
+                Button(action: {}){
+                    Image(systemName: "plus")
+                        .accessibilityLabel("New Scrum")
+                }
+            }
     }
 }
 
