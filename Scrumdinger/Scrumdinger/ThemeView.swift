d5b6b707b0a068c45a3b35dd4c962c9f060aae37
Ahsan Abbasi <1abbasia@gmail.com>
2022-08-10 17:44:29 -0400

    started theme picker view for edit view

---
+++ Scrumdinger/Scrumdinger/ThemeView.swift
@@ -0,0 +1,23 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Ahsan on 8/10/22.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct ThemeView: View{
+    var body: some View{
+        Text("Theme picker")
+    }
+}
+
+
+struct ThemeView_Previews: PreviewProvider {
+    static var previews: some View{
+        ThemeView()
+    }
+}
