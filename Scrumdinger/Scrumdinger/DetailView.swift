a36218079dee01b842cfa68a36100ac5ea734313
Ahsan Abbasi <1abbasia@gmail.com>
2022-08-10 18:53:27 -0400

    issue with update call paramter

--- Scrumdinger/Scrumdinger/DetailView.swift
+++ Scrumdinger/Scrumdinger/DetailView.swift
@@ -9,23 +9,26 @@ import Foundation
 import SwiftUI
 
 struct DetailView: View {
-    @State private var isPresentingEditingView = false
-    let scrum: DailyScrum
-    var body: some View{
+    @Binding var scrum: DailyScrum
+    
+    @State private var data = DailyScrum.Data()
+    @State private var isPresentingEditView = false
+    
+    var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
-                NavigationLink(destination: MeetingView()){
-                Label("Start Meeting", systemImage: "timer")
-                    .font(.headline)
-                    .foregroundColor(.accentColor)
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
                 }
-                HStack{
+                HStack {
                     Label("Length", systemImage: "clock")
                     Spacer()
                     Text("\(scrum.lengthInMinutes) minutes")
                 }
                 .accessibilityElement(children: .combine)
-                HStack{
+                HStack {
                     Label("Theme", systemImage: "paintpalette")
                     Spacer()
                     Text(scrum.theme.name)
@@ -45,35 +48,36 @@ struct DetailView: View {
         .navigationTitle(scrum.title)
         .toolbar {
             Button("Edit") {
-                isPresentingEditingView = true
+                isPresentingEditView = true
+                data = scrum.data
             }
         }
-        .sheet(isPresented: $isPresentingEditingView){
+        .sheet(isPresented: $isPresentingEditView) {
             NavigationView {
-                DetailEditView()
+                DetailEditView(data: $data)
                     .navigationTitle(scrum.title)
                     .toolbar {
-                        ToolbarItem(placement: .cancellationAction){
-                            Button("Cancel"){
-                                isPresentingEditingView = false
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
                             }
-                            
                         }
-                        ToolbarItem(placement: .confirmationAction){
-                            Button("Done"){
-                                isPresentingEditingView = false
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Done") {
+                                isPresentingEditView = false
+                                scrum.update(from: data)
                             }
                         }
                     }
+            }
         }
     }
 }
 
-struct DetailView_Previews: PreviewProvider{
-    static var previews: some View{
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
         NavigationView {
-        DetailView(scrum: DailyScrum.sampleData[0])
+            DetailView(scrum: .constant(DailyScrum.sampleData[0]))
         }
     }
-}
 }
