c068b91913cc48183872c761544f01f2438e37a3
Ahsan Abbasi <1abbasia@gmail.com>
2022-08-15 22:01:16 -0400

    added scrumtimer model file, downloaded from starter project

--- Scrumdinger/Scrumdinger/MeetingHeaderView.swift
+++ Scrumdinger/Scrumdinger/MeetingHeaderView.swift
@@ -29,6 +29,7 @@ struct MeetingHeaderView: View {
         
         VStack {
         ProgressView(value: progress)
+                .progressViewStyle(ScrumProgressViewStyle(theme: theme))
             
                 HStack {
                     VStack(alignment: .leading) {
@@ -41,12 +42,14 @@ struct MeetingHeaderView: View {
                         Text("Seconds Remaining")
                             .font(.caption)
                         Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
+                            .labelStyle(.trailingIcon)
                     }
                 }
         }
         .accessibilityElement(children: .ignore)
         .accessibilityLabel("Time remaining")
         .accessibilityValue("\(minutesRemaining) minutes")
+        .padding([.top, .horizontal])
         
     }
 }
